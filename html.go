a41db9fac6f95bbf680cbe853d049a5c157d5dc8
Vilmara Pereira da Silva <vilmara.silva@pagar.me>
2024-08-26 22:01:32 -0300

    Create html.go

---
+++ html.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// <-chan - canal somente-leitura
+
+func titulo(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+
+		}(url)
+	}
+	return c
+
+}
